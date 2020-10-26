d14150e0bbb3b44fdc1488f49b060e22081b9e6d
Solkan <solkanmahesh@gmail.com>
2020-10-26 23:00:31 +0530

    Solkan added two method in mah1

--- src/main/java/Interview_Programs/strings/mah1.java
+++ src/main/java/Interview_Programs/strings/mah1.java
@@ -1,4 +1,14 @@
 package Interview_Programs.strings;
 
-public class mah1 {
+public class mah1
+{
+
+    public static void methid1()
+    {
+        System.out.println();
+    }
+    public static void methid2()
+    {
+        System.out.println();
+    }
 }
